188bbc5cfce08f9a5b4692c16c321c19ba338eaf
YukiWatanabe824 <gyggg950@gmail.com>
2024-07-16 23:42:41 +0900

    数値型のチェック

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const greeting: string = "hello"
-const target: string = "world"
+const width1 = 5
+const width2 = 8
+const height: number = 3
+const area = (width1 + width2) * height / 2
+console.log(area)
+
+console.log(0.1 + 0.2)
