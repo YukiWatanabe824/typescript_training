1c213e6683df4eb9f6f2a51834863f5863023d30
YukiWatanabe824 <gyggg950@gmail.com>
2024-07-18 05:52:49 +0900

    TSの数値リテラルでは進数表記がそのままできる

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-const width1 = 5
-const width2 = 8
-const height: number = 3
-const area = (width1 + width2) * height / 2
-console.log(area)
+const binary = 0b1010
+const octal = 0o755
+const hexadecimal = 0xff
 
-console.log(0.1 + 0.2)
+console.log(binary, octal, hexadecimal)
