2c722f7ba377dd38d397d73e6326728323aefad3
YukiWatanabe824 <gyggg950@gmail.com>
2024-07-22 13:48:26 +0900

    ==による暗黙の型変化が行われることがあるので==は使用しないほうがよい

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { createInterface } from 'readline'
+// import { createInterface } from 'readline'
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
 
-rl.question('パスワードを入力:', (password) => {
-  if (password === "123") {
-    console.log("yes")
-  } else {
-    console.log("no")
-  }
-  rl.close
-})
+// rl.question('パスワードを入力:', (password) => {
+//   if (password === "123") {
+//     console.log("yes")
+//   } else {
+//     console.log("no")
+//   }
+//   rl.close
+// })
+
+const str: any = "3"
+console.log(str == 3)
+console.log(str === 3)
